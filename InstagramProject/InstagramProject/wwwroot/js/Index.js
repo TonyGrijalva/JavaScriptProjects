30250cf2ee6f9870883f8a1132dc583e2cd7dc49
Marcos Grijalva <shonish04@gmail.com>
2021-03-12 11:43:03 -0700

    Midterm resubmission

--- InstagramProject/InstagramProject/wwwroot/js/Index.js
+++ InstagramProject/InstagramProject/wwwroot/js/Index.js
@@ -10,14 +10,19 @@ loginButton.innerHTML = "Log in";
 
 div.appendChild(loginButton);
 
+var a = document.createElement("a");
+a.setAttribute("href", "Home/Registration");
 var signUpButton = document.createElement("button");
-signUpButton.innerHTML = "Sign in";
+signUpButton.innerHTML = "Sign up";
 
-div.appendChild(signUpButton);
+a.appendChild(signUpButton);
+
+div.appendChild(a);
 
 var lineBreak = document.createElement("br");
 
 div.appendChild(lineBreak);
+
 
 var table = document.createElement("table");
 var tbody = document.createElement("tbody");
@@ -60,39 +65,13 @@ div.appendChild(lineBreak2);
 
 "use strict";
 
-//var product = new Product("Product", "Description", "Image", 25);
+var product = new Image("Product", "Description", "Image", 25);
 
-//var product1 = new Product("Product 1", "Description 1", "Image 1", 5);
-//var product2 = new Product("Product 2", "Description 2", "Image 2", 10);
-//var product3 = new Product("Product 3", "Description 3", "Image 3", 15);
-//var product4 = new Product("Product 4", "Description 4", "Image 4", 20);
-//var product5 = new Product("Product 5", "Description 5", "Image 5", 25);
-//var product6 = new Product("Product 6", "Description 6", "Image 6", 30);
-//var product7 = new Product("Product 7", "Description 7", "Image 7", 35);
-//var product8 = new Product("Product 8", "Description 8", "Image 8", 40);
-//var product9 = new Product("Product 9", "Description 9", "Image 9", 45);
-//var product10 = new Product("Product 10", "Description 10", "Image 10", 50);
-//var product11 = new Product("Product 11", "Description 11", "Image 11", 55);
-//var product12 = new Product("Product 12", "Description 12", "Image 12", 60);
-//var product13 = new Product("Product 13", "Description 13", "Image 13", 65);
-//var product14 = new Product("Product 14", "Description 14", "Image 14", 70);
-//var product15 = new Product("Product 15", "Description 15", "Image 15", 75);
+var image1 = new Image();
+image1.imageName = "Image 1";
+image1.imageId = 1;
+image1.imagePath = "Any path";
 
-//var cart = [];
-//cart.push(product1);
-//cart.push(product2);
-//cart.push(product3);
-//cart.push(product4);
-//cart.push(product5);
-//cart.push(product6);
-//cart.push(product7);
-//cart.push(product9);
-//cart.push(product10);
-//cart.push(product11);
-//cart.push(product12);
-//cart.push(product13);
-//cart.push(product14);
-//cart.push(product15);
 
 
 
@@ -100,17 +79,17 @@ div.appendChild(lineBreak2);
  
 
 
-//var image = new Object(); {
-////    imageName: "This", imageDescription: "Image", imagePrice: 30
-//};
+var image = new Object(); {
+//    imageName: "This", imageDescription: "Image", imagePrice: 30
+};
 
 
-//var imageArray = document.getElementsByClassName("CustomImages");
-//for (var x = 0; x < imageArray.length; x++) {
-//    imageArray[x].onclick = function () {
-//        alert("I am an image");
-//    }
-//}
+var imageArray = document.getElementsByClassName("CustomImages");
+for (var x = 0; x < imageArray.length; x++) {
+    imageArray[x].onclick = function () {
+        alert("I am an image");
+    }
+}
 
 
 
